f60939ff042789ec2ff0f68297c821becccfc5fb
Beth <iyara@materiamagica.com>
2020-03-11 20:55:02 +0000

    set up basic server

--- server.py
+++ server.py
@@ -1,5 +1,25 @@
 import socket
-from _thread import *
-import _pickle as pickle
 import time
+
+
+server = ""
+port = 5556
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+    print(e)
+
+s.listen()
+print("Server started, waiting for a connection.")
+
+while True:
+    conn, addr = s.accept()
+    print("[CONNECT] New Connection!")
+
+
+
 
