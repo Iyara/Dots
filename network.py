b3678e342f1200d9b92989367990f3cf1c4948f6
Beth <iyara@materiamagica.com>
2020-03-11 22:32:04 -0400

    start menu works and gets player name

--- network.py
+++ network.py
@@ -4,6 +4,7 @@ import socket
 class Network:
 	def __init__(self):
 		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#self.client.settimeout(10.0)
 		self.server = "44.233.18.91"
 		self.port = 5555
 		self.addr = (self.server, self.port)
